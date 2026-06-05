39495ef29f9eec44c5f22eea4b075111ecabb213
Artabarean <atabarea@c3r14s4.42madrid.com>
2026-06-05 13:06:10 +0200

    ex00 halfway done

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: atabarea <atabarea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/01 11:35:29 by atabarea          #+#    #+#             */
-/*   Updated: 2026/06/04 12:54:40 by atabarea         ###   ########.fr       */
+/*   Updated: 2026/06/05 13:04:53 by atabarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,22 +31,63 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other)
 	return (*this);
 }
 
+void ScalarConverter::tochar(std::string input)
+{
+	size_t len = input.length();
+	int res = 0;
+	int sign = 1;
+	if (input[0] == '-')
+		sign *= -1;
+	for(size_t i = 0; i < len; i++)
+	{
+		if (input[i] >= '0' && input[i] <= '9')
+			res = res * 10 + (input[i] - '0');
+	}
+	if (res != 0)
+	{
+		res *= sign;
+		if (res < 0 || res > 127)
+		{
+			std::cout << "char: impossible" << std::endl;
+			return;
+		}
+		if (!std::isprint(res))
+		{
+			std::cout << "char: non displayable" << std::endl;
+			return;
+		}
+		std::cout << "char: " << static_cast<char>(res) << std::endl;
+		return;
+	}
+	if (input.length() > 1)
+	{
+		std::cout << "char: impossible" << std::endl;
+		return;
+	}
+	if (!std::isprint(std::strtod(input.c_str(), NULL)))
+	{
+		std::cout << "char: non displayable" << std::endl;
+		return;
+	}
+	std::cout << "char: " << static_cast<char>(input[0]) << std::endl;
+}
+
 void ScalarConverter::convert(std::string input)
 {
-	// std::cout << "char: " << char(input[0]) << std::endl;
-	// std::cout << "int: " << int(input[0]) << std::endl;
-	// std::cout << "char: impossible" << std::endl;
-	// std::cout << "int: impossible" << std::endl;
-	for (size_t i = 0; i < input.length(); i++)
-	{
-		if (input[i] < '0' && input[i] > '9')
-			break;
-		if (i == input.length())
-		{
-			input[++i] = '.';
-			input[++i] = '0';
-		}
-	}
-	input[input.length()] = 'f';
-	std::cout << "float: " << input << std::endl;
+	ScalarConverter::tochar(input);
+	//		convert input to double
+	//		if more than 127 impossible
+	//		if >31 && <127 
+	//			static cast to char
+	//			print char
+	//		else: non displayable
+	// toint
+	//		parse: if more than 1 char, impossible
+	//		convert input to double
+	//		if <INT_MIN or >INT_MAX impossible
+	//			static cast to int
+	//			print int
+	//		
+	// tofloat
+	// todouble
 }
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -6,18 +6,25 @@
 /*   By: atabarea <atabarea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/01 10:55:10 by atabarea          #+#    #+#             */
-/*   Updated: 2026/06/04 12:54:19 by atabarea         ###   ########.fr       */
+/*   Updated: 2026/06/05 12:55:11 by atabarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
+#include <cstdlib>
+#include <cctype>
+#include <stdexcept>
 
 class ScalarConverter
 {
 	public:
 		ScalarConverter& operator=(const ScalarConverter &);
 		void static convert(std::string);
+		void static tochar(std::string);
+		void static toint(std::string);
+		void static tofloat(std::string);
+		void static todouble(std::string);
 		
 	private:
 		ScalarConverter(void);
