c2d7d5e3c678064502f4c2991662c2f5f4a4250c
Artabarean <artabarean@gmail.com>
2026-06-10 11:40:02 +0000

    gixes

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -33,9 +33,14 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other)
 
 void ScalarConverter::tochar(std::string input)
 {
+	size_t start = input.find(".");
+	size_t end = input.length() - start;
+	if (start != std::string::npos)
+		input.erase(start, end);
 	size_t len = input.length();
 	int res = 0;
 	int sign = 1;
+	std::cout << input << std::endl;
 	for(size_t i = 0; i < len; i++)
 	{
 		if (i == 0 && input[i] == '-')
@@ -77,6 +82,10 @@ void ScalarConverter::tochar(std::string input)
 
 void ScalarConverter::toint(std::string input)
 {
+	size_t start = input.find(".");
+	size_t end = input.length() - start;
+	if (start != std::string::npos)
+		input.erase(start, end);
 	size_t len = input.length();
 	unsigned long long res= 0;
 	int sign = 1;
@@ -108,16 +117,41 @@ void ScalarConverter::toint(std::string input)
 	std::cout << res << std::endl;
 }
 
+void ScalarConverter::tofloat(std::string input)
+{
+	if (input == "nan" || input == "nanf" || input == "+inff" || input == "+inf" || input == "-inff" || input == "-inf")
+	{
+		if (input != "nanf" && input != "+inff" &&input != "-inff")
+		input.append("f");
+		std::cout << "float: " << input << std::endl;
+		return;
+	}
+	size_t len = input.length();
+	unsigned long long res= 0;
+	int sign = 1;
+	for (size_t i = 0; i < len; i++)
+	{
+		if (i == 0 && input[i] == '-' && len > 1)
+		{
+			++i;
+			sign *= -1;
+		}
+		if (isalpha(input[i]) && (len - 1) > i)
+		{
+			std::cout << "float: impossible" << std::endl;
+			return;
+		}
+		if (isdigit(input[i]))
+			res = res * 10 + (static_cast<int>(input[i]) - '0');
+		else
+			res = static_cast<int>(input[i]);
+	}
+}
+
 void ScalarConverter::convert(std::string input)
 {
 	ScalarConverter::tochar(input);
 	ScalarConverter::toint(input);
-	//		parse: if more than 1 char, impossible
-	//		convert input to double
-	//		if <INT_MIN or >INT_MAX impossible
-	//			static cast to int
-	//			print int
-	//		
-	// tofloat
+	ScalarConverter::tofloat(input);
 	// todouble
 }
